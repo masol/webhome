f99851676d5d6ca62d213ee485320eefbe61a76c
masol <masol.li@gmail.com>
2014-01-24 01:11:25 +0800

    re #11277 first skeleton code.

---
+++ src/webhome.js
@@ -0,0 +1,12 @@
+/**************************************************************************
+*  This file is part of the WebHome.
+*  Published under the MIT license.
+*  Copyright (C) by SanPolo Co.Ltd.
+*
+*  See https://osc.spolo.org/ for more information.
+*  SanPolo Co.Ltd
+**************************************************************************/
+
+/** Because we are multiple cooperate, this file can been divide to multi-file. 
+* and merge through a js compiler to generate final file.
+**/
